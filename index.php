d35a1c50a2220c2a1b3738b32fd0bc3f52f4b3ec
nitin123chaudhary <49305005+nitin123chaudhary@users.noreply.github.com>
2020-10-29 10:42:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include "header.php";
+
+?>
+<h1>Welcome to the website.</h1>
+        <p><b>Please read the side bar for important information !</b></p>
+
+        <p>This is an experiment and its still in the alpha stage.</p>
+        <p>Please refer to the Contact Us page to submit issues regarding the website.</p>
+      </div>
+    </div>
+    <style>
+   #content
+    {
+    	font-size: 17px;	
+    }
+</style>
+<?php
+  include "footer.php";
+?>
